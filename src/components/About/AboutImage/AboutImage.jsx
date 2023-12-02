f8113ab5e7a482dc2aed7735bb89d0b3c9a587e7
Eduardo <c.eduardocarvalhoc@gmail.com>
2023-12-02 08:32:46 -0300

    initial archives

---
+++ src/components/About/AboutImage/AboutImage.jsx
@@ -0,0 +1,12 @@
+import DevImage from '../../../assets/dev_photo.jpeg'
+
+
+function AboutImage(){
+    return(
+        <>
+            <img src={DevImage} alt="" className='about-image'/>
+        </>
+    )
+}
+
+export default AboutImage
---
+++ src/components/About/AboutTitle/AboutTitle.jsx
@@ -0,0 +1,9 @@
+function AboutTitle(){
+    return(
+        <>
+            <h2>About <span className="orange">Me</span></h2>
+        </>
+    )
+}
+
+export default AboutTitle
