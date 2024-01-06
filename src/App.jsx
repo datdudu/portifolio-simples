58ee3c245766ca699898b424dee8bdb13c829158
Eduardo <c.eduardocarvalhoc@gmail.com>
2024-01-05 21:19:49 -0300

    Adding routes to the components created and creating a components that agregate all of them

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
 import './App.css'
 import Header from './views/Header.jsx'
-import About from './views/About.jsx'
-import Service from './views/Service.jsx'
-import Skill from './views/Skill.jsx'
+import Home from './views/Home.jsx'
 import Footer from './views/Footer.jsx'
-import { Outlet } from 'react-router-dom'
+
 function App() {
 
   return (
     <>
       <Header />
-      <Outlet />
-      <About />
-      <Service />
-      <Skill />
+      <Home />
       <Footer />
     </>
   )
